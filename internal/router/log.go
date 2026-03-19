ef47b3b3c9a1737636d232d9702f8d2bbcc748d3
Massimo Ronca <walterstucco@gmail.com>
2026-03-19 14:43:12 +0100

    refactor: simplify deduplicated log output and message handling in router logger

--- internal/router/log.go
+++ internal/router/log.go
@@ -6,41 +6,36 @@ import (
 	"sync"
 )
 
-const dedupThreshold = 100
-
 type dedupLogger struct {
 	mu      sync.Mutex
-	lastKey string
+	lastMsg string
 	count   int
 }
 
 var logger = &dedupLogger{}
 
 func logEntry(host, ssid string, action string, ruleMatched bool) {
-	key := fmt.Sprintf("ssid=%q → %s", ssid, action)
+	var msg string
+	if ruleMatched {
+		msg = fmt.Sprintf("[router] host=%s ssid=%q → %s", host, ssid, action)
+	} else {
+		msg = fmt.Sprintf("[router] ssid=%q → %s", ssid, action)
+	}
 
 	logger.mu.Lock()
 	defer logger.mu.Unlock()
 
-	if key == logger.lastKey {
+	if msg == logger.lastMsg {
 		logger.count++
-		if logger.count%dedupThreshold == 0 {
-			log.Printf("[router] %s repeated %d times", key, logger.count)
-		}
 		return
 	}
 
-	// Flush leftover count for previous key
-	if logger.count > 0 && logger.count%dedupThreshold != 0 {
-		log.Printf("[router] %s repeated %d times", logger.lastKey, logger.count)
+	// Flush repeat count before printing new message
+	if logger.count > 0 {
+		log.Printf("[router] Last log repeated %d times", logger.count)
 	}
 
-	logger.lastKey = key
+	logger.lastMsg = msg
 	logger.count = 0
-
-	if ruleMatched {
-		log.Printf("[router] host=%s %s", host, key)
-	} else {
-		log.Printf("[router] %s", key)
-	}
+	log.Print(msg)
 }
